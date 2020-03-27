2bf5234820b5d02a4aaba970dcbbfd8c9a689310
emanuellemoco <emanuellesmoco@gmail.com>
2020-03-27 19:48:32 -0300

    funcionando botao pause

--- src/main.c
+++ src/main.c
@@ -18,6 +18,8 @@
 /************************************************************************/
 
 #include "asf.h"
+#include "musicas.h"
+// #include "sysfont.h"
 
 /************************************************************************/
 /* defines                                                              */
@@ -57,7 +59,7 @@
 	#define BUT_PIO_IDX		  11
 	#define BUT_PIO_IDX_MASK (1u << BUT_PIO_IDX)
 
-	// Configuracoes do botoes plaquinha
+	// Configuracoes do botoes e led plaquinha
 	#define BUT1_PIO			PIOD
 	#define BUT1_PIO_ID		16
 	#define BUT1_PIO_IDX		28
@@ -67,243 +69,23 @@
 	#define BUT2_PIO_ID		12
 	#define BUT2_PIO_IDX		31
 	#define BUT2_PIO_IDX_MASK	(1u << BUT2_PIO_IDX)
+
 	#define BUT3_PIO			PIOA
 	#define BUT3_PIO_ID		10
 	#define BUT3_PIO_IDX		19
 	#define BUT3_PIO_IDX_MASK	(1u << BUT3_PIO_IDX)
 	
-	volatile char but_flag;
+	#define LED_PIO_ID ID_PIOC
+	#define LED_PIO PIOC
+	#define LED_PIN 8
+	#define LED_PIN_MASK (1 << LED_PIN)
 
-	//Defining note frequency
-	#define NOTE_C4  262   
-	#define NOTE_D4  294
-	#define NOTE_E4  330
-	#define NOTE_F4  349
-	#define NOTE_G4  392
-	#define NOTE_A4  440
-	#define NOTE_B4  494
-	#define NOTE_C5  523
-	#define NOTE_D5  587
-	#define NOTE_E5  659
-	#define NOTE_F5  698
-	#define NOTE_G5  784
-	#define NOTE_A5  880
-	#define NOTE_B5 988
-	#define c  261
-	#define d  294
-	#define e  329
-	#define f  349
-	#define g  391
-	#define gS  415
-	#define a  440
-	#define aS  455
-	#define b  466
-	#define cH  523
-	#define cSH  554
-	#define dH  587
-	#define dSH  622
-	#define eH  659
-	#define fH  698
-	#define fSH  740
-	#define gH  784
-	#define gSH  830
-	#define aH  880
-	#define NOTE_B0  31
-	#define NOTE_C1  33
-	#define NOTE_CS1 35
-	#define NOTE_D1  37
-	#define NOTE_DS1 39
-	#define NOTE_E1  41
-	#define NOTE_F1  44
-	#define NOTE_FS1 46
-	#define NOTE_G1  49
-	#define NOTE_GS1 52
-	#define NOTE_A1  55
-	#define NOTE_AS1 58
-	#define NOTE_B1  62
-	#define NOTE_C2  65
-	#define NOTE_CS2 69
-	#define NOTE_D2  73
-	#define NOTE_DS2 78
-	#define NOTE_E2  82
-	#define NOTE_F2  87
-	#define NOTE_FS2 93
-	#define NOTE_G2  98
-	#define NOTE_GS2 104
-	#define NOTE_A2  110
-	#define NOTE_AS2 117
-	#define NOTE_B2  123
-	#define NOTE_C3  131
-	#define NOTE_CS3 139
-	#define NOTE_D3  147
-	#define NOTE_DS3 156
-	#define NOTE_E3  165
-	#define NOTE_F3  175
-	#define NOTE_FS3 185
-	#define NOTE_G3  196
-	#define NOTE_GS3 208
-	#define NOTE_A3  220
-	#define NOTE_AS3 233
-	#define NOTE_B3  247
-	#define NOTE_C4  262
-	#define NOTE_CS4 277
-	#define NOTE_D4  294
-	#define NOTE_DS4 311
-	#define NOTE_E4  330
-	#define NOTE_F4  349
-	#define NOTE_FS4 370
-	#define NOTE_G4  392
-	#define NOTE_GS4 415
-	#define NOTE_A4  440
-	#define NOTE_AS4 466
-	#define NOTE_B4  494
-	#define NOTE_C5  523
-	#define NOTE_CS5 554
-	#define NOTE_D5  587
-	#define NOTE_DS5 622
-	#define NOTE_E5  659
-	#define NOTE_F5  698
-	#define NOTE_FS5 740
-	#define NOTE_G5  784
-	#define NOTE_GS5 831
-	#define NOTE_A5  880
-	#define NOTE_AS5 932
-	#define NOTE_B5  988
-	#define NOTE_C6  1047
-	#define NOTE_CS6 1109
-	#define NOTE_D6  1175
-	#define NOTE_DS6 1245
-	#define NOTE_E6  1319
-	#define NOTE_F6  1397
-	#define NOTE_FS6 1480
-	#define NOTE_G6  1568
-	#define NOTE_GS6 1661
-	#define NOTE_A6  1760
-	#define NOTE_AS6 1865
-	#define NOTE_B6  1976
-	#define NOTE_C7  2093
-	#define NOTE_CS7 2217
-	#define NOTE_D7  2349
-	#define NOTE_DS7 2489
-	#define NOTE_E7  2637
-	#define NOTE_F7  2794
-	#define NOTE_FS7 2960
-	#define NOTE_G7  3136
-	#define NOTE_GS7 3322
-	#define NOTE_A7  3520
-	#define NOTE_AS7 3729
-	#define NOTE_B7  3951
-	#define NOTE_C8  4186
-	#define NOTE_CS8 4435
-	#define NOTE_D8  4699
-	#define NOTE_DS8 4978
-	
-	int mario_theme_notes[] = { //by Prince @ princetronics.com
-		NOTE_E7, NOTE_E7, 0, NOTE_E7,
-		0, NOTE_C7, NOTE_E7, 0,
-		NOTE_G7, 0, 0,  0,
-		NOTE_G6, 0, 0, 0,
-			
-		NOTE_C7, 0, 0, NOTE_G6,
-		0, 0, NOTE_E6, 0,
-		0, NOTE_A6, 0, NOTE_B6,
-		0, NOTE_AS6, NOTE_A6, 0,
-			
-		NOTE_G6, NOTE_E7, NOTE_G7,
-		NOTE_A7, 0, NOTE_F7, NOTE_G7,
-		0, NOTE_E7, 0, NOTE_C7,
-		NOTE_D7, NOTE_B6, 0, 0,
-			
-		NOTE_C7, 0, 0, NOTE_G6,
-		0, 0, NOTE_E6, 0,
-		0, NOTE_A6, 0, NOTE_B6,
-		0, NOTE_AS6, NOTE_A6, 0,
-			
-		NOTE_G6, NOTE_E7, NOTE_G7,
-		NOTE_A7, 0, NOTE_F7, NOTE_G7,
-		0, NOTE_E7, 0, NOTE_C7,
-		NOTE_D7, NOTE_B6, 0, 0
-	};
-	//Mario main them tempo by Prince
-	int mario_theme_tempo[] = {
-		83, 83, 83, 83,
-		83, 83, 83, 83,
-		83, 83, 83, 83,
-		83, 83, 83, 83,
-			
-		83, 83, 83, 83,
-		83, 83, 83, 83,
-		83, 83, 83, 83,
-		83, 83, 83, 83,
-			
-		112, 112, 112,
-		83, 83, 83, 83,
-		83, 83, 83, 83,
-		83, 83, 83, 83,
-			
-		83, 83, 83, 83,
-		83, 83, 83, 83,
-		83, 83, 83, 83,
-		83, 83, 83, 83,
-			
-		112, 112, 112,
-		83, 83, 83, 83,
-		83, 83, 83, 83,
-		83, 83, 83, 83,
-	};
-	
-	//Underworld melody
-	int underworld_notes[] = {
-		NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
-		NOTE_AS3, NOTE_AS4, 0,
-		0,
-		NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
-		NOTE_AS3, NOTE_AS4, 0,
-		0,
-		NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
-		NOTE_DS3, NOTE_DS4, 0,
-		0,
-		NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
-		NOTE_DS3, NOTE_DS4, 0,
-		0, NOTE_DS4, NOTE_CS4, NOTE_D4,
-		NOTE_CS4, NOTE_DS4,
-		NOTE_DS4, NOTE_GS3,
-		NOTE_G3, NOTE_CS4,
-		NOTE_C4, NOTE_FS4, NOTE_F4, NOTE_E3, NOTE_AS4, NOTE_A4,
-		NOTE_GS4, NOTE_DS4, NOTE_B3,
-		NOTE_AS3, NOTE_A3, NOTE_GS3,
-		0, 0, 0
-	};
-	//Underwolrd tempo
-	int underworld_tempo[] = {
-		83, 83, 83, 83,
-		83, 83, 166,
-		333,
-		83, 83, 83, 83,
-		83, 83, 166,
-		333,
-		83, 83, 83, 83,
-		83, 83, 166,
-		333,
-		83, 83, 83, 83,
-		83, 83, 166,
-		166, 55, 55, 55,
-		166, 166,
-		166, 166,
-		166, 166,
-		55, 55, 55, 55, 55, 55,
-		100, 100, 100,
-		100, 100, 100,
-		333, 333, 333
-	};
-	//marcha imperial by nicksort @github
-	int imperial_march_notes[] = {
-		a,a,a,f,cH,a,f,cH,a,0,eH,eH,eH,fH,cH,gS,f,cH,a,0
-	};
 
-	int imperial_march_tempo[] = {
-		500,500,500,350,150,500,350,150,650,500,500,500,500,350,150,500,350,150,650,500
-	};
+	volatile char pause;
+	volatile char but1_flag;
+	volatile char but2_flag;
+	volatile char but3_flag;
+
 	
 /************************************************************************/
 /* constants                                                            */
@@ -320,7 +102,7 @@
 void init(void);
 void playMusic(int tempo[], int notes[], int size);
 void pio_enable_interrupt(Pio *p_pio, const uint32_t ul_mask);
-
+void LED_init(int estado);
 
 /************************************************************************/
 /* interrupcoes                                                         */
@@ -331,9 +113,17 @@ void pio_enable_interrupt(Pio *p_pio, const uint32_t ul_mask);
 /************************************************************************/
 
 void but_callBack(void){
-	but_flag = 1;
+	pause = !pause;
 }
-
+void but1_callBack(void){
+	but1_flag = 1;
+}
+void but2_callBack(void){
+	but2_flag = 1;
+}
+void but3_callBack(void){
+	but3_flag = 1;
+}
 // Função de inicialização do uC e config correta dos perifericos e pinos
 void init(void)
 {
@@ -369,15 +159,21 @@ void init(void)
 	// configura pino ligado ao botão como entrada com um pull-up.
 	pio_configure(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK, PIO_PULLUP);
 //	pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, PIO_DEFAULT);
-	pio_set_input(BUT1_PIO, BUT1_PIO_IDX_MASK, PIO_DEFAULT); //??? PIO DEFAULT?
-	pio_set_input(BUT2_PIO, BUT2_PIO_IDX_MASK, PIO_DEFAULT);
-	pio_set_input(BUT3_PIO_ID, BUT3_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_set_input(BUT1_PIO, BUT1_PIO_IDX_MASK, PIO_PULLUP); //??? PIO DEFAULT?
+	pio_set_input(BUT2_PIO, BUT2_PIO_IDX_MASK, PIO_PULLUP);
+	pio_set_input(BUT3_PIO_ID, BUT3_PIO_IDX_MASK, PIO_PULLUP);
 	
 	//?
 	// Configura interrupção no pino referente ao botao e associa
 	// função de callback caso uma interrupção for gerada
 	// a função de callback é a: but_callback()
-	pio_handler_set(BUT_PIO, BUT_PIO_ID, BUT_PIO_IDX_MASK, PIO_IT_FALL_EDGE, but_callBack);
+	pio_handler_set(BUT_PIO, BUT_PIO_ID, BUT_PIO_IDX_MASK, PIO_IT_RISE_EDGE, but_callBack);
+	pio_handler_set(BUT1_PIO, BUT1_PIO_ID, BUT1_PIO_IDX_MASK, PIO_IT_FALL_EDGE, but1_callBack);
+	pio_handler_set(BUT2_PIO, BUT2_PIO_ID, BUT2_PIO_IDX_MASK, PIO_IT_FALL_EDGE, but2_callBack);
+	pio_handler_set(BUT3_PIO, BUT3_PIO_ID, BUT3_PIO_IDX_MASK, PIO_IT_FALL_EDGE, but3_callBack);
+
+
+
 	
 	//Ativar o pull-up
 	//pio_pull_up(BUT_PIO, BUT_PIO_IDX_MASK, 1);
@@ -388,36 +184,59 @@ void init(void)
 	//?
 	// Ativa interrupção
 	pio_enable_interrupt(BUT_PIO, BUT_PIO_IDX_MASK);
+	pio_enable_interrupt(BUT1_PIO, BUT1_PIO_IDX_MASK);
+	pio_enable_interrupt(BUT2_PIO, BUT2_PIO_IDX_MASK);
+	pio_enable_interrupt(BUT3_PIO, BUT3_PIO_IDX_MASK);
+
 	
 	// Configura NVIC para receber interrupcoes do PIO do botao
 	// com prioridade 4 (quanto mais próximo de 0 maior)
 	NVIC_EnableIRQ(BUT_PIO_ID);
-	NVIC_SetPriority(BUT_PIO_ID, 4); // Prioridade 4
+	NVIC_EnableIRQ(BUT1_PIO_ID);
+	NVIC_EnableIRQ(BUT2_PIO_ID);
+	NVIC_EnableIRQ(BUT3_PIO_ID);
+
+	NVIC_SetPriority(BUT_PIO_ID, 3); // Prioridade 4
+	NVIC_SetPriority(BUT1_PIO_ID, 4); 
+	NVIC_SetPriority(BUT2_PIO_ID, 4);
+	NVIC_SetPriority(BUT3_PIO_ID, 4);
 	
 	
 }
+
+//Inicializa o pino do LED
+void LED_init(int estado){
+	pmc_enable_periph_clk(LED_PIO_ID);
+	pio_set_output(LED_PIO, LED_PIN_MASK, estado, 0, 0);
+};
 
 /************************************************************************/
 /* Main                                                                 */
 /************************************************************************/
 
+
 void playMusic(int tempo[], int notes[], int size){
+	//ver se o botao flag = 1 -> pausa
 	for (int i=0 ;  i< size ; i++){
 		//se a nota for 0, toca o 0 (silencio) pelo tempo
 		if (notes[i] == 0 ){
 			delay_ms(tempo[i]);
 		}
-		
+		//nota diferente de 0
 		else {
 			float t_delay = 1000.0 /(int)(notes[i])	;
 			pio_clear(PIOC, LED_PIO_IDX_MASK);    // Coloca 0 no pino do LED
 			for (long j=0 ; j < (tempo[i]/t_delay ); j++){
+				if (pause){
+					j--;
+				} else {
 				if (tempo[i]!= 0){
 					pio_set(PIOC, BUZ_PIO_IDX_MASK);
 					delay_us(t_delay*1000.0);
 					pio_clear(PIOC, BUZ_PIO_IDX_MASK);
 					delay_us(t_delay*1000.0);
 				}
+			}
 			}
 		}
 		delay_us(85); //para dar um pause entre cada nota
@@ -432,6 +251,20 @@ int main(void)
 {
 	// inicializa sistema e IOs
 	init();
+	but1_flag = 0;
+	but2_flag = 0;
+	but3_flag = 0;
+	pause = 0;
+
+	// // Init OLED
+	// gfx_mono_ssd1306_init();
+
+	// /* Configura Leds */
+	// LED_init(0);
+
+	// gfx_mono_draw_string("Escolha", 50,16, &sysfont);
+
+
 	// super loop
 	// aplicacoes embarcadas não devem sair do while(1).
 	
@@ -446,20 +279,23 @@ int main(void)
 	pio_set(LED2_PIO, LED2_PIO_IDX_MASK);
 	pio_set(LED3_PIO, LED3_PIO_IDX_MASK);
 		
-	if (but_flag){
+	if (but1_flag){
 		pio_clear(LED1_PIO, LED1_PIO_IDX_MASK); //liga led
 		playMusic(mario_theme_tempo, mario_theme_notes, mario_theme_size);
 		pio_set(LED1_PIO, LED1_PIO_IDX_MASK); //desliga led
-		
+		but1_flag = 0;
+	}
+	if (but2_flag){
 		pio_clear(LED2_PIO, LED2_PIO_IDX_MASK);
-		playMusic(imperial_march_tempo, imperial_march_notes, imperial_march_size);
-		pio_set(LED2_PIO, LED2_PIO_IDX_MASK);
-		
+	 	playMusic(imperial_march_tempo, imperial_march_notes, imperial_march_size);
+	 	pio_set(LED2_PIO, LED2_PIO_IDX_MASK);		
+		but2_flag = 0;
+	}
+	if (but3_flag){
 		pio_clear(LED3_PIO, LED3_PIO_IDX_MASK);
-		playMusic(underworld_tempo, underworld_notes, underworld_size);
-		pio_set(LED3_PIO, LED3_PIO_IDX_MASK);
-			
-		but_flag = 0;
+	 	playMusic(underworld_tempo, underworld_notes, underworld_size);
+	 	pio_set(LED3_PIO, LED3_PIO_IDX_MASK);
+		but3_flag = 0;
 	}
 	
 	pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
